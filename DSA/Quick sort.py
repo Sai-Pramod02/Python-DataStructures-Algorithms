a6035f63ea944be77d82c49a92f3930fd046ef36
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2023-05-19 00:06:11 +0530

    Initial Commit

---
+++ DSA/Quick sort.py
@@ -0,0 +1,22 @@
+def partitionIndex(arr,low,high):
+    pivot = arr[low]
+    i=low
+    j=high
+    while i<j:
+        while arr[i]<=pivot and i<=high-1:
+            i+=1
+        while arr[j]>pivot and j>=low+1:
+            j-=1
+        if i<j:
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[low],arr[j]=arr[j],arr[low]
+    return j
+
+def quickSort(arr,low,high):
+    if low<=high:
+        pIndex = partitionIndex(arr,low,high)
+        quickSort(arr,low,pIndex-1)
+        quickSort(arr,pIndex+1,high)
+    return arr
+
+print(quickSort([4,6,2,5,7,9,1,3],0,7))
